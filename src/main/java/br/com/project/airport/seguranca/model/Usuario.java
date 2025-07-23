62972f385036b3c9f43781dd6356602ef4d4d727
Willian <willianmassuki@gmail.com>
2025-07-23 12:54:48 -0300

    ajuste na camada de controle de voo, camada gestao de recurso

---
+++ src/main/java/br/com/project/airport/seguranca/model/Usuario.java
@@ -0,0 +1,40 @@
+package br.com.project.airport.seguranca.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idUsuario;
+
+    private String nome;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer idUsuario, String nome) {
+        this.idUsuario = idUsuario;
+        this.nome = nome;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
